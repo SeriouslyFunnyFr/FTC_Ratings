b7777e8b34169779b1a1f13b9f069f7db4d39f2c
SeriouslyFunnyFr <ari@borisland.com>
2025-01-30 23:17:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,38 +3,58 @@ from utils.ratings import update_ratings
 
 app = Flask(__name__)
 
-# In-memory storage for teams and their ratings
+# In-memory storage for teams, matches, and ratings
 teams = {}  # Format: {'Team A': 1000, 'Team B': 1000, ...}
+matches = []  # Format: [{'game': 1, 'blue_alliance': ['Team A', 'Team B'], 'red_alliance': ['Team C', 'Team D'], 'blue_score': None, 'red_score': None}, ...]
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/record_match', methods=['GET', 'POST'])
-def record_match():
+@app.route('/add_teams', methods=['GET', 'POST'])
+def add_teams():
     if request.method == 'POST':
-        # Get team names and scores
-        alliance1_team1 = request.form['alliance1_team1']
-        alliance1_team2 = request.form['alliance1_team2']
-        alliance2_team1 = request.form['alliance2_team1']
-        alliance2_team2 = request.form['alliance2_team2']
-        alliance1_score = int(request.form['alliance1_score'])
-        alliance2_score = int(request.form['alliance2_score'])
+        team_name = request.form['team_name']
+        if team_name and team_name not in teams:
+            teams[team_name] = 1000  # Default rating for new teams
+    return render_template('add_teams.html', teams=teams.keys())
 
-        # Add new teams if they don't exist
-        for team in [alliance1_team1, alliance1_team2, alliance2_team1, alliance2_team2]:
-            if team not in teams:
-                teams[team] = 1000  # Default rating for new teams
+@app.route('/schedule_matches', methods=['GET', 'POST'])
+def schedule_matches():
+    if request.method == 'POST':
+        game_number = len(matches) + 1
+        blue_alliance = [request.form['blue_team1'], request.form['blue_team2']]
+        red_alliance = [request.form['red_team1'], request.form['red_team2']]
+        matches.append({
+            'game': game_number,
+            'blue_alliance': blue_alliance,
+            'red_alliance': red_alliance,
+            'blue_score': None,
+            'red_score': None,
+        })
+    return render_template('schedule_matches.html', teams=teams.keys(), matches=matches)
 
-        # Update ratings using TrueSkill
-        teams[alliance1_team1], teams[alliance1_team2], teams[alliance2_team1], teams[alliance2_team2] = update_ratings(
-            teams[alliance1_team1], teams[alliance1_team2],
-            teams[alliance2_team1], teams[alliance2_team2],
-            alliance1_score, alliance2_score
+@app.route('/record_scores', methods=['GET', 'POST'])
+def record_scores():
+    if request.method == 'POST':
+        game_number = int(request.form['game_number'])
+        blue_score = int(request.form['blue_score'])
+        red_score = int(request.form['red_score'])
+
+        # Update the match with scores
+        match = matches[game_number - 1]
+        match['blue_score'] = blue_score
+        match['red_score'] = red_score
+
+        # Update team ratings
+        blue_team1, blue_team2 = match['blue_alliance']
+        red_team1, red_team2 = match['red_alliance']
+        teams[blue_team1], teams[blue_team2], teams[red_team1], teams[red_team2] = update_ratings(
+            teams[blue_team1], teams[blue_team2],
+            teams[red_team1], teams[red_team2],
+            blue_score, red_score
         )
-        return redirect(url_for('rankings'))
-
-    return render_template('record_match.html', teams=teams.keys())
+    return render_template('record_scores.html', matches=matches)
 
 @app.route('/rankings')
 def rankings():
